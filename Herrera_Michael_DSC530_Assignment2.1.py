4c0bb03631aac04fc0d3a4948ee71caff35d2ccf
Herrera714 <85767249+Herrera714@users.noreply.github.com>
2021-09-12 20:45:42 -0700

    Add files via upload

---
+++ Herrera_Michael_DSC530_Assignment2.1.py
@@ -0,0 +1,36 @@
+# Michael Herrera
+# DSC 530
+# Assignment 2.1 Preparing for Exploratory Data Analysis
+
+
+# Display Text
+print("Hello World! I wonder why that is always the default coding text to start with")
+
+# Adding Two Numbers
+print(2 + 2)
+
+# Subtract two numbers
+print(60 - 50)
+
+# Multiply Two Numbers
+print(50 * 2)
+
+# Divide Two Numbers
+print(1000 / 2)
+
+# Concatenate two strings together
+print("Michael " + "Herrera")
+ 
+# Creating a list of 4 items
+a = ['Lists', 25 , 30 , 35 , 40 ]
+print(a)
+
+# Appending an item to this list
+lst = [10,20,30,40] 
+x = [0,1,2] 
+lst.append(x) 
+print(lst) 
+
+# Created a tuple with 4 items
+Thistuple = ("apple", "banana", "cherry")
+print(Thistuple)
